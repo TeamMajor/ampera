168e54bb1243d0d6b4019ccbbe887c1c02b18963
wahyu <wahyu.wahyu45@gmail.com>
2017-05-12 15:02:35 +0700

    perbaikan token

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,7 +17,7 @@ class Welcome extends CI_Controller {
 		);
 		//print_r($data['budaya']);
     foreach($data['budaya'] as $value){ 
-	print_r($value);
+	//print_r($value);
       if(preg_match('/\s/',$value['nama'])==1){ 
        $replacedata = preg_replace('/\s+/', '@', $value['nama']); 
        $kalimat = str_replace($value['nama'],$replacedata,$kalimat);
@@ -30,8 +30,8 @@ class Welcome extends CI_Controller {
     $value = str_replace("@"," ",$value);
     $tanda['newinformasi'][] = $value;
    }
-		$this->getpos($tanda);
-	//print_r($tanda['newinformasi']);
+		//$this->getpos($tanda);
+	print_r($tanda['newinformasi']);
 	}
 }
 
